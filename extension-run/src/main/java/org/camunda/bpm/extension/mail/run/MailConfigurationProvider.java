6d245acd21477b0060a5d95a966783b408c2368e
shaarmann <stephan.haarmann@camunda.com>
2022-08-01 11:46:52 +0200

    feat: Add support for Spring Boot and Camunda Run.
    - Wrap the connectors configuration in a Bean to be configurable via
      application.yml
    - Provide a Camunda Platform 7 plugin that registers the connectors and
      that sets the configuration to the bean
    
    Closes #44

---
+++ extension-run/src/main/java/org/camunda/bpm/extension/mail/run/MailConfigurationProvider.java
@@ -0,0 +1,22 @@
+package org.camunda.bpm.extension.mail.run;
+
+import org.camunda.bpm.extension.mail.config.PropertiesMailConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+@Configuration
+@ConfigurationProperties(prefix="plugin.mail")
+public class MailConfigurationProvider extends PropertiesMailConfiguration {
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public Properties getProperties() {
+        return this.properties;
+    }
+}
---
+++ extension-run/src/main/java/org/camunda/bpm/extension/mail/run/MailConnectorPlugin.java
@@ -0,0 +1,34 @@
+package org.camunda.bpm.extension.mail.run;
+
+import org.camunda.bpm.engine.impl.cfg.AbstractProcessEnginePlugin;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.extension.mail.MailConnectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Component
+public class MailConnectorPlugin extends AbstractProcessEnginePlugin {
+    @Autowired
+    protected MailConfigurationProvider mailConfigurationProvider;
+
+    private final Logger LOGGER = Logger.getLogger(this.getClass().getName());
+
+
+    @Override
+    public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
+        LOGGER.info("Configuring Mail Connectors");
+        MailConnectors.deleteMails().setConfiguration(mailConfigurationProvider);
+        MailConnectors.pollMails().setConfiguration(mailConfigurationProvider);
+        MailConnectors.sendMail().setConfiguration(mailConfigurationProvider);
+    }
+
+    public MailConfigurationProvider getMailConfigurationProvider() {
+        return mailConfigurationProvider;
+    }
+
+    public void setMailConfigurationProvider(MailConfigurationProvider mailConfiguration) {
+        this.mailConfigurationProvider = mailConfiguration;
+    }
+}
