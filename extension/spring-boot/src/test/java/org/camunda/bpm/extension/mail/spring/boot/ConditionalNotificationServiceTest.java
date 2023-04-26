2f02952143eb402ce075519e67a2240120c0d524
Jonathan Lukas <jonathan.lukas@camunda.com>
2023-04-26 12:56:09 +0200

    Add option to disable notification service in Spring Boot

---
+++ extension/spring-boot/src/test/java/org/camunda/bpm/extension/mail/spring/boot/ConditionalNotificationServiceTest.java
@@ -0,0 +1,32 @@
+package org.camunda.bpm.extension.mail.spring.boot;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.camunda.bpm.extension.mail.notification.MailNotificationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+public class ConditionalNotificationServiceTest {
+  private final ApplicationContextRunner contextRunner =
+      new ApplicationContextRunner().withUserConfiguration(MailConnectorConfiguration.class);
+
+  @Test
+  void shouldNotInstantiateNotificationService() {
+    contextRunner
+        .withPropertyValues("camunda.bpm.plugin.mail.notification.enabled=false")
+        .run(
+            context -> {
+              assertThat(context).doesNotHaveBean(MailNotificationService.class);
+            });
+  }
+
+  @Test
+  void shouldNotInstantiateNotificationService2() {
+    contextRunner
+        .withPropertyValues("camunda.bpm.plugin.mail.mail.notification.enabled=false")
+        .run(
+            context -> {
+              assertThat(context).doesNotHaveBean(MailNotificationService.class);
+            });
+  }
+}
