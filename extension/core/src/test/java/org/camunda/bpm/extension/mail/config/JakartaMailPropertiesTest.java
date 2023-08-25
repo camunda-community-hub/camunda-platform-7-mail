e023a15a38368e9b46087f5450c8706f31d18771
Jonathan Lukas <jonathan.lukas@camunda.com>
2023-08-25 07:24:52 +0200

    format

--- extension/core/src/test/java/org/camunda/bpm/extension/mail/config/JakartaMailPropertiesTest.java
+++ extension/core/src/test/java/org/camunda/bpm/extension/mail/config/JakartaMailPropertiesTest.java
@@ -1,17 +1,17 @@
 package org.camunda.bpm.extension.mail.config;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-
-import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
 
 public class JakartaMailPropertiesTest {
   @Test
   public void testLoadPropertiesFromClasspath_absolutePath() throws IOException {
-    try(InputStream in = JakartaMailProperties.getPropertiesAsStream("classpath:/mail-config.properties")){
+    try (InputStream in =
+        JakartaMailProperties.getPropertiesAsStream("classpath:/mail-config.properties")) {
       assertThat(in).isNotNull();
       Properties properties = new Properties();
       properties.load(in);
@@ -21,7 +21,8 @@ public class JakartaMailPropertiesTest {
 
   @Test
   public void testLoadPropertiesFromClasspath_relativePath() throws IOException {
-    try(InputStream in = JakartaMailProperties.getPropertiesAsStream("classpath:mail-config.properties")){
+    try (InputStream in =
+        JakartaMailProperties.getPropertiesAsStream("classpath:mail-config.properties")) {
       assertThat(in).isNotNull();
       Properties properties = new Properties();
       properties.load(in);
